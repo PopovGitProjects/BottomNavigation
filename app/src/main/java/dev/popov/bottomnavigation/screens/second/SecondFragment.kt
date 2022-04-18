e1467a858c2c307da6fe8f85d406679b7465f520
Sergey Popov <popov.workmail@gmail.com>
2022-04-18 17:56:29 +0500

    Initial commit

---
+++ app/src/main/java/dev/popov/bottomnavigation/screens/second/SecondFragment.kt
@@ -0,0 +1,25 @@
+package dev.popov.bottomnavigation.screens.second
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.popov.bottomnavigation.R
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            SecondFragment()
+    }
+}
