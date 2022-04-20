593dd4a11129bc3ec5c21c211853cf0bd576b3e4
Sergey Popov <popov.workmail@gmail.com>
2022-04-20 16:10:45 +0500

    почему то не работает Bottom menu

--- app/src/main/java/dev/popov/bottomnavigation/MainActivity.kt
+++ app/src/main/java/dev/popov/bottomnavigation/MainActivity.kt
@@ -1,14 +1,25 @@
 package dev.popov.bottomnavigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import dev.popov.bottomnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.botNavView.setupWithNavController(navController)
+        binding.botNavView.selectedItemId = R.id.item2
     }
 }
